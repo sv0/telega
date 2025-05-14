06ed875f637e6e23c18618276a909a2e3dc9128c
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2025-05-14 11:28:36 +0200

    test: change user_info
    return telegram user info

--- telega/test_default.py
+++ telega/test_default.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from .fixtures import client
+from .fixtures import client  # noqa
 
 
-def test_get_home_page(client):
-    # expected_response = {'message': 'Hello, World!'}
+def test_get_home_page(client):  # noqa
     result = client.simulate_get('/')
     assert 'telega API' in result.text
 
@@ -13,8 +12,19 @@ def test_get_home_page(client):
 @pytest.fixture(scope='function')
 def user_info():
     return {
-        'email': 'KUNGFU.PANDA@EXAMPLE.COM',
-        'id': 123,
-        'job_title': 'Senior Engineer',
-        'name': 'Panda,Kungfu',
+        "id": 7100802653,
+        "username": None,
+        "usernames": None,
+        "first_name": "Amanda",
+        "last_name": "Wilson",
+        "fake": False,
+        "verified": False,
+        "premium": False,
+        "mutual_contact": False,
+        "bot": False,
+        "bot_chat_history": False,
+        "restricted": False,
+        "restriction_reason": None,
+        "user_was_online": "Last seen recently",
+        "phone": "380979424929"
     }
