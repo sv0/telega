751f7bdee83ccdf46ced10bebcbfc06030c833c6
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2025-11-09 11:30:28 +0100

    style(PEP8): fix `E501 line too long (91 > 79 characters)`

--- telega/config.py
+++ telega/config.py
@@ -21,6 +21,8 @@ class Config:
         self.telegram_api_hash = os.getenv('TELEGRAM_API_HASH')
         self.telegram_api_phone_number = os.getenv('TELEGRAM_API_PHONE_NUMBER')
         self.telegram_session_dc_id = os.getenv('TELEGRAM_SESSION_DC_ID')
-        self.telegram_session_server_address = os.getenv('TELEGRAM_SESSION_SERVER_ADDRESS')
+        self.telegram_session_server_address = os.getenv(
+            'TELEGRAM_SESSION_SERVER_ADDRESS'
+        )
         self.telegram_session_port = os.getenv('TELEGRAM_SESSION_PORT', 443)
         self.telegram_session_auth_key = os.getenv('TELEGRAM_SESSION_AUTH_KEY')
